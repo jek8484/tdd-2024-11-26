372fdb31d0d4edd9a034e54b58952d9ed6cb8d44
jek8484 <ysms3611@gmail.com>
2024-11-26 11:38:46 +0900

    work

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -9,7 +9,7 @@ public class SimpleCalculator {
         return num1 - num2;
     }
 
-    public int multiply(int i, int i1) {
-        return 50;
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
     }
 }
